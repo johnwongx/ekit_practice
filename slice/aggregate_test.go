f9577a2b63177395fa18b2cf0da434f7a9347c8c
JhonWong <1290773033@qq.com>
2023-08-30 10:45:32 +0800

    补充aggregate测试用例

--- slice/aggregate_test.go
+++ slice/aggregate_test.go
@@ -1,9 +1,10 @@
 package slice
 
 import (
+	ekitpractice "ekit_practice"
 	"testing"
 
-	"github.com/magiconair/properties/assert"
+	"github.com/stretchr/testify/assert"
 )
 
 type Integer int
@@ -32,6 +33,26 @@ func TestMax(t *testing.T) {
 			assert.Equal(t, res, tc.wantVal)
 		})
 	}
+
+	assert.Panics(t, func() {
+		Max[int](nil)
+	})
+	assert.Panics(t, func() {
+		Max[int]([]int{})
+	})
+
+	testMaxTypes[int](t)
+	testMaxTypes[int8](t)
+	testMaxTypes[int16](t)
+	testMaxTypes[int32](t)
+	testMaxTypes[int64](t)
+	testMaxTypes[uint](t)
+	testMaxTypes[uint8](t)
+	testMaxTypes[uint16](t)
+	testMaxTypes[uint32](t)
+	testMaxTypes[uint64](t)
+	testMaxTypes[float32](t)
+	testMaxTypes[float64](t)
 }
 
 func TestMin(t *testing.T) {
@@ -58,6 +79,26 @@ func TestMin(t *testing.T) {
 			assert.Equal(t, res, tc.wantVal)
 		})
 	}
+
+	assert.Panics(t, func() {
+		Min[int](nil)
+	})
+	assert.Panics(t, func() {
+		Min[int]([]int{})
+	})
+
+	testMinTypes[int](t)
+	testMinTypes[int8](t)
+	testMinTypes[int16](t)
+	testMinTypes[int32](t)
+	testMinTypes[int64](t)
+	testMinTypes[uint](t)
+	testMinTypes[uint8](t)
+	testMinTypes[uint16](t)
+	testMinTypes[uint32](t)
+	testMinTypes[uint64](t)
+	testMinTypes[float32](t)
+	testMinTypes[float64](t)
 }
 
 func TestSum(t *testing.T) {
@@ -66,6 +107,9 @@ func TestSum(t *testing.T) {
 		src     []Integer
 		wantVal Integer
 	}{
+		{
+			name: "nil",
+		},
 		{
 			name:    "empty",
 			src:     []Integer{},
@@ -89,4 +133,32 @@ func TestSum(t *testing.T) {
 			assert.Equal(t, res, tc.wantVal)
 		})
 	}
+
+	testSumTypes[int](t)
+	testSumTypes[int8](t)
+	testSumTypes[int16](t)
+	testSumTypes[int32](t)
+	testSumTypes[int64](t)
+	testSumTypes[uint](t)
+	testSumTypes[uint8](t)
+	testSumTypes[uint16](t)
+	testSumTypes[uint32](t)
+	testSumTypes[uint64](t)
+	testSumTypes[float32](t)
+	testSumTypes[float64](t)
+}
+
+func testMaxTypes[T ekitpractice.RealNumber](t *testing.T) {
+	res := Max[T]([]T{1, 2, 3})
+	assert.Equal(t, res, T(3))
+}
+
+func testMinTypes[T ekitpractice.RealNumber](t *testing.T) {
+	res := Min[T]([]T{1, 2, 3})
+	assert.Equal(t, res, T(1))
+}
+
+func testSumTypes[T ekitpractice.RealNumber](t *testing.T) {
+	res := Sum[T]([]T{1, 2, 3})
+	assert.Equal(t, res, T(6))
 }
